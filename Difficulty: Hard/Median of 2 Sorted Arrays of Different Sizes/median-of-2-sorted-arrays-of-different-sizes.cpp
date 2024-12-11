d6bed725279017b17f53e09b0d2071c6e675b774
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2024-12-11 19:07:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    double medianOf2(vector<int>& a, vector<int>& b) {
+        // Your code goes here
+        int n=a.size()+b.size();
+        double res=0;
+        vector<int>merged(n);
+        merge(a.begin(),a.end(),b.begin(),b.end(),merged.begin());
+        if(n%2!=0){
+            res=merged[n/2];
+        }
+        else{
+            res=(merged[(n-1)/2]+merged[(n)/2])/2.0;
+            
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading `t`
+
+    while (t--) {
+        // Read the first array
+        string line1;
+        getline(cin, line1);
+        stringstream ss1(line1);
+
+        vector<int> array1;
+        int num;
+        while (ss1 >> num) {
+            array1.push_back(num);
+        }
+
+        // Read the second array
+        string line2;
+        getline(cin, line2);
+        stringstream ss2(line2);
+
+        vector<int> array2;
+        while (ss2 >> num) {
+            array2.push_back(num);
+        }
+
+        // Create a Solution object and calculate the median
+        Solution ob;
+        if (array2.size() == 1 && array2[0] == 0) {
+            array2.pop_back();
+        }
+
+        double ans = ob.medianOf2(array1, array2);
+        if (ans == (int)ans) {
+            cout << (int)ans << endl;
+        } else {
+            cout << ans << endl;
+        }
+
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
